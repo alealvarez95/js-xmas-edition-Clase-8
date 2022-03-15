39f28d60bfe0200235b8359ca53e50b95fe99dd8
alealvarez95 <alejandroalvarezm2@gmail.com>
2022-03-15 19:16:54 -0300

    agrega validaciones
    Se agregan mas validaciones a las ya existentes

--- js/main.js
+++ js/main.js
@@ -8,27 +8,27 @@ const descripcionRegalo = $formulario["descripcion-regalo"].value;
 function validarNombre(nombre) {
     if (nombre.length === 0) {
         return "Este campo debe tener al menos 1 caracter";
-    }
-
-    if (nombre.length >= 50) {
+    } else if (nombre.length >= 50) {
         return "Este campo debe tener menos de 50 caracteres";
+    } else {
+        return "";
     }
-
-    return "";
 }
 
 function validarCiudad(ciudad) {
     if (ciudad.length === 0) {
         return "Este campo debe tener al menos 1 caracter";
+    } else {
+        return "";
     }
-
-    return "";
 }
 
 function validarDescripcionRegalo(descripcionRegalo) {
     if (descripcionRegalo.length === 0) {
         return "Este campo debe tener al menos 1 caracter";
+    } else if (descripcionRegalo.length >= 100) {
+        return "Este campo debe tener menos de 100 caracteres";
+    } else {
+        return "";
     }
-
-    return "";
 }
--- js/pruebas.js
+++ js/pruebas.js
@@ -5,18 +5,31 @@ function probarValidarNombre() {
         validarNombre("111111111111111111111111111111111111111111111111111111111111111111111111111111111111111") === "Este campo debe tener menos de 50 caracteres",
         "Validar nombre no validó que el nombre sea menor a 50 caracteres"
     );
+
+    console.assert(validarNombre("Prueba") === "", "Validar nombre fallo con un nombre valido")
 }
 
 probarValidarNombre();
 
 function probarValidarCiudad() {
     console.assert(validarCiudad("") === "Este campo debe tener al menos 1 caracter", "Validar ciudad no validó que el nombre no sea vacío");
+
+    console.assert(validarCiudad("Bs. As.") === "", "Validar ciudad fallo con una ciudad valida")
 }
 
 probarValidarCiudad();
 
 function probarValidarDescripcionRegalo() {
     console.assert(validarDescripcionRegalo("") === "Este campo debe tener al menos 1 caracter", "Validar descripcion regalo no validó que el nombre no sea vacío");
+
+    console.assert(
+        validarDescripcionRegalo(
+            "111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111"
+        ) === "Este campo debe tener menos de 100 caracteres",
+        "Validar descripcion no validó que la descripcion sea menor a 100 caracteres"
+    );
+
+    console.assert(validarDescripcionRegalo("Esto es una prueba") === "", "Validar descripcion fallo con una descripcion valida")
 }
 
 probarValidarDescripcionRegalo();
